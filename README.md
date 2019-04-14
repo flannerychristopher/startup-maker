f9d94ab1ae9a59e47600af4ae776730b02dc8ea4
flannerychristopher <flannerychristopher@gmail.com>
2019-04-14 16:00:13 -0400

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-see on gh-pages
-<a href="https://flannerychristopher.github.io/startup-maker/">
-  https://flannerychristopher.github.io/startup-maker/
-</a>
---
+++ precache-manifest.728074722950f04298adc0de3e7756a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27519a6401e89f3995a8",
+    "url": "/startup-maker/static/js/runtime~main.e7b091a3.js"
+  },
+  {
+    "revision": "50ce16927ee600ba9894",
+    "url": "/startup-maker/static/js/main.dc253c7c.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/startup-maker/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "50ce16927ee600ba9894",
+    "url": "/startup-maker/static/css/main.93bcf678.chunk.css"
+  },
+  {
+    "revision": "da6c782443f3affe78ad9746d07096f4",
+    "url": "/startup-maker/index.html"
+  }
+];
